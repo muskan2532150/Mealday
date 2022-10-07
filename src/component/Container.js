20e1d6d0c4fe753d48883ed1ebadf6f5ec640e6e
muskan2532150 <rg2532150@gmail.com>
2022-10-07 14:49:52 +0530

    Added ScrollBar in home page

--- src/component/Container.js
+++ src/component/Container.js
@@ -1,17 +1,26 @@
-import React, { useEffect } from 'react';
+import { useEffect,useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchdata } from '../redux/Container/Containers';
 import Item from './Item';
+import {AiOutlineArrowUp} from 'react-icons/ai';
 
 const Container = () => {
-  const { data } = useSelector((state) => state.container);
+const { data } = useSelector((state) => state.container);
+const [scroll, setscroll] = useState(document.documentElement.scrollTop);
 
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(fetchdata());
+    window.onscroll=()=>setscroll(document.documentElement.scrollTop);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+const handleScroll = () =>{
+  document.body.scrollTop=0;
+  document.documentElement.scrollTop=0;  
+  setscroll(0);
+    
+}
 
   let count = 0;
   return (
@@ -20,6 +29,10 @@ const Container = () => {
         count += 1;
         return <Item record={record} key={record.id} count={count} />;
       })}
+      <div className='topbtn' >
+        <button className={ scroll > 20 ? 'scrollbtn ' : 'scrollDown'} type='button' onClick={()=>handleScroll()}><AiOutlineArrowUp /></button>
+      </div>
+      {console.log(scroll)}
     </div>
   );
 };
--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { FaMicrophone } from 'react-icons/fa';
 import { AiFillSetting } from 'react-icons/ai';
-
+import {GiFruitBowl} from 'react-icons/gi';
 const Navbar = () => (
   <nav>
-    <Link to="/" />
+    <Link to="/" ><GiFruitBowl/></Link>
     <div>
       <ul>
         <li><FaMicrophone /></li>

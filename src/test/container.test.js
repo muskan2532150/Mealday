9b6f84dc02fa6b8d9da8ec1d4413fb9d12a06c2d
muskan2532150 <rg2532150@gmail.com>
2022-10-07 21:50:08 +0530

    Added test for Component

---
+++ src/test/container.test.js
@@ -0,0 +1,58 @@
+import { render } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from '../redux/store';
+import Item from '../component/Item';
+import Li from '../component/Li';
+import Popup from '../component/Popup';
+import Container from '../component/Container';
+
+const fruit = {
+    genus: "Fragaria",
+    name: "Strawberry",
+    id: 3,
+    family: "Rosaceae",
+    order: "Rosales",
+    nutritions: {
+        carbohydrates: 5.5,
+        protein: 0,
+        fat: 0.4,
+        calories: 29,
+        sugar: 5.4
+    }
+};
+
+test('Demo test', () => {
+  expect((1 + 2)).toBe(3);
+});
+
+test('renders container Component', () => {
+  const con = render(
+  <BrowserRouter>
+  <Provider store={store}>
+    <Container/>
+    </Provider>
+  </BrowserRouter>
+    );
+  expect(con).toMatchSnapshot();
+});
+
+test('renders Item Component', () => {
+  const item = render(
+    <BrowserRouter>
+    <Provider store={store}>
+      <Item record={fruit} key={fruit.id} count={0}  />
+    </Provider>
+    </BrowserRouter>
+    ,
+  );
+  expect(item).toMatchSnapshot();
+});
+
+test('renders LI Component', () => {
+  const li = render(<Provider store={store}>
+    <Li nutritions={fruit.nutritions}/>
+    </Provider>);
+  expect(li).toMatchSnapshot();
+});
+
